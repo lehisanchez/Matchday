a6fc9d4ad49f40e8b6a180f87ab5c748b2c4b54f
Lehi Sanchez <sanchez.lehi@gmail.com>
2017-05-07 08:17:55 -0400

    Added User Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  
+  get 'dashboard' => 'dashboard#index', as: 'dashboard'
+
+  devise_for :users
+
+  authenticated :user do
+    get '/', to: redirect('/dashboard')
+  end
+
   resources :leagues,   except: [:new, :edit, :index], format: false
   resources :teams,     except: [:new, :edit, :index], format: false
   resources :matches,   except: [:new, :edit, :index, :show], format: false
   resources :standings, except: [:new, :edit, :index, :show], format: false
+  
   root 'welcome#index'
 end
